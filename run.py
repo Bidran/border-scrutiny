4c21dbc7978969902db4e5c89d82bbc83486802c
Bidran <>
2023-10-04 21:31:40 +0000

    Comment the functions and adjust character length

--- run.py
+++ run.py
@@ -4,68 +4,84 @@ import sys
 import random
 from random import choice
 
-def typingPrint(text):
-  for character in text:
-    sys.stdout.write(character)
-    sys.stdout.flush()
-    time.sleep(0.01)
 
+def typingPrint(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+
+"""
+Various lists from which functions will pull data
+"""
 
 first_name_men = [
-    'Dmitri','Mikhail','Anton','Pavel','Andrei','Ivan',
-    'Sergei','Fyodor'
-    ]
+    'Dmitri', 'Mikhail', 'Anton', 'Pavel', 'Andrei', 'Ivan',
+    'Sergei', 'Fyodor']
 
 first_name_women = [
-    'Yelena','Natalia','Svetlana','Irina','Ekaterina',
-    'Olga','Yulia','Ana'
-    ]
+    'Yelena', 'Natalia', 'Svetlana', 'Irina', 'Ekaterina',
+    'Olga', 'Yulia', 'Ana']
 
 last_name_men = [
-    'Petrovich','Romanov','Kovalenko','Sokolov','Volkov',
-    'Nikitin','Voronin','Romanovich',
+    'Petrovich', 'Romanov', 'Kovalenko', 'Sokolov', 'Volkov',
+    'Nikitin', 'Voronin', 'Romanovich',
     ]
 
 last_name_women = [
-    'Ivanova','Kuznetsova','Zhdanova','Popova','Romanovna',
-    'Ilyina','Yegorova','Titova'
+    'Ivanova', 'Kuznetsova', 'Zhdanova', 'Popova', 'Romanovna',
+    'Ilyina', 'Yegorova', 'Titova'
     ]
 
 origin_country = [
-    'Novoslavia','Tashkarsk','Dushkovia','Arktovia','Kazakhrad',
-    'Bolskaya','Sputnikstan','Molotovia'
-    ] 
-
-possession = [
-    'Knife','Gun','Lighter','Cigarettes','Phone',
-    'Chocolate','Crisps','Juice','Painting','Headphones','Laptop',
-    'USB Stick', 'Broom', 'Rollerblades','Fidget Spinner','Lightbulb'
+    'Novoslavia', 'Tashkarsk', 'Dushkovia', 'Arktovia', 'Kazakhrad',
+    'Bolskaya', 'Sputnikstan', 'Molotovia'
     ]
 
+possession = [
+    'Knife', 'Gun', 'Lighter', 'Cigarettes', 'Phone',
+    'Chocolate', 'Crisps', 'Juice', 'Painting', 'Headphones', 'Laptop',
+    'USB Stick', 'Broom', 'Rollerblades', 'Fidget Spinner', 'Lightbulb'
+    ]
+
+
 def makePerson():
+
+    """
+    Chooses a random choice between a man and a woman
+    functions and returns the choice
+    """
+
     persons = [makeMan(), makeWoman()]
     chosen = random.choice(persons)
     return chosen
-    
+
 
 def makeMan():
-    global random_fname,random_lname,age,random_country,random_possession
+    """
+    Pulls random values from lists which will create a profile of a man
+    """
+    global random_fname, random_lname, age, random_country, random_possession
     random_fname = random.choice(first_name_men)
     random_lname = random.choice(last_name_men)
-    age = random.choice(range(10,100))
+    age = random.choice(range(10, 100))
     random_country = random.choice(origin_country)
     random_possession = random.choice(possession)
-    return random_fname,random_lname,age,random_country,random_possession
-    
+    return random_fname, random_lname, age, random_country, random_possession
+
+
 def makeWoman():
-    global random_fname,random_lname,age,random_country,random_possession
+    """
+    Pulls random values from lists which will create a profile of a woman
+    """
+    global random_fname, random_lname, age, random_country, random_possession
     random_fname = random.choice(first_name_women)
     random_lname = random.choice(last_name_women)
-    age = random.choice(range(10,100))
+    age = random.choice(range(10, 100))
     random_country = random.choice(origin_country)
     random_possession = random.choice(possession)
-    return random_fname,random_lname,age,random_country,random_possession
-    
+    return random_fname, random_lname, age, random_country, random_possession
 
 
 def newLine():
@@ -74,12 +90,21 @@ def newLine():
     """
     print('\n')
 
+
+"""
+ANSI color values which are used troughout the code
+"""
 red = '\033[1;31;40m'
 white = '\033[1;37;40m'
 green = '\033[1;32;40m'
 yellow = '\033[1;33;40m'
 
+
 def randomRule():
+    """
+    Pulls the value from lists and creates a profile of a person and saves it
+    to global variables which will be used in other parts of the program
+    """
     global random_fname_chosen
     global random_lname_chosen
     global random_country_chosen
@@ -93,21 +118,25 @@ def randomRule():
     if random_fname_chosen == random_fname_man:
         random_lname_chosen = random_lname_man
     else:
-       random_lname_chosen = random_lname_woman
+        random_lname_chosen = random_lname_woman
     random_country_chosen = random.choice(origin_country)
-    random_age = random.choice(range(10,100))
+    random_age = random.choice(range(10, 100))
     newLine()
     typingPrint('Alright, the rule for today is: \n')
     typingPrint("Don't let anyone trough who has any of these: \n")
     newLine()
-    typingPrint(f"First name: {red}{random_fname_chosen}{white} \nLast name: {red}{random_lname_chosen}{white} \nCountry: {red}{random_country_chosen}{white} \nAge: {red}{random_age} years old{white}\n")
-    typingPrint(f'And remember, never let in anyone who has a {red}Knife{white} or a {red}Gun{white}.\n')
-    return random_fname_chosen
-    return random_lname_chosen
-    return random_country_chosen
-    return random_age
+    typingPrint(f"First name: {red}{random_fname_chosen}{white} \n"
+                f"Last name: {red}{random_lname_chosen}{white} \n"
+                f"Country: {red}{random_country_chosen}{white} \n"
+                f"Age: {red}{random_age} years old{white}\n")
+    typingPrint(f'And remember, never let in anyone who has a {red}Knife'
+                f"{white} or a {red}Gun{white}.\n")
+
 
 def firstPerson():
+    """
+    Creates a person which is used to compare attributes with forbidden ones
+    """
     global first_person_name
     global first_person_surname
     global first_person_age
@@ -120,25 +149,34 @@ def firstPerson():
     first_person_country = first_person[3]
     first_person_possession = first_person[4]
     newLine()
-    print(f'{yellow} _______________________________________________________\n')
-    print(f"|  |{white}Name: {first_person_name} {first_person_surname}{yellow}|     |{white}Country: {first_person_country}{yellow}|\n")
+    print(f'{yellow} __________________________________________________'
+          f'_____\n')
+    print(f"|  |{white}Name: {first_person_name} {first_person_surname}"
+          f"{yellow}|     |{white}Country: {first_person_country}{yellow}|\n")
     print('|------------------------------------------------------|\n')
-    print(f"|  |{white}Age: {first_person_age}{yellow}|                   |{white}Possession: {first_person_possession}{yellow}|\n"),
+    print(f"|  |{white}Age: {first_person_age}{yellow}|"
+          f"                   |{white}"
+          f"Possession: {first_person_possession}{yellow}|\n"),
     print(f'|______________________________________________________|{white}\n')
 
+
+# Money value, used as score
 money = 0
+
 
 def beginGame():
     """
-    Takes players input if he wishes to proceed with the game.
+    Takes players input if they wish to proceed with the game or end it.
     """
     newLine()
-    user_input = input(f'Are you ready to begin? ({green}yes{white}/{red}no{white}): \n')
+    user_input = input(f'Are you ready to begin?'
+                       f' ({green}yes{white}/{red}no{white}): \n')
     if user_input.lower() == 'yes':
         randomRule()
     elif user_input.lower() == 'no':
         typingPrint("Off to jail with you! \n")
-        user_input_no = input(f'Did you change your mind? ({green}yes{white}/{red}no{white}): \n')
+        user_input_no = input(f'Did you change your mind?'
+                              f'({green}yes{white}/{red}no{white}): \n')
         if user_input_no.lower() == 'yes':
             beginGame()
         elif user_input_no.lower() == 'no':
@@ -151,50 +189,77 @@ def beginGame():
         print('Type yes or no')
         beginGame()
 
+
 def letTrough():
+    """
+    Function which compares values between original random person and each
+    consecutive one after which it gives {money} value accordingly.
+    Called upon when replying yes.
+    """
     global money
     if (random_fname_chosen == first_person_name):
-        print(f"{red}You've let trough a person with forbidden first name!\n-$20{white}")
+        print(f"{red}You've let trough a person with forbidden first name!"
+              f"\n-$20{white}")
         money = money - 20
     elif (random_lname_chosen == first_person_surname):
-        print(f"{red}You've let trough a person with forbidden last name!\n-$20{white}")
+        print(f"{red}You've let trough a person with forbidden last name!"
+              f"\n-$20{white}")
         money = money - 20
     elif (random_age == first_person_age):
-        print(f"{red}You've let trough a person with forbidden age!\n-$20{white}")
+        print(f"{red}You've let trough a person with forbidden age!"
+              f"\n-$20{white}")
         money = money - 20
     elif (random_country_chosen == first_person_country):
-        print(f"{red}You've let trough a person with forbidden origin country!\n-$20{white}")
+        print(f"{red}You've let trough a person with forbidden origin country!"
+              f"\n-$20{white}")
         money = money - 20
-    elif (first_person_possession == 'Gun' or first_person_possession == 'Knife'):
-        print(f"{red}You've let trough a person with forbidden possessions!\n-$20{white}")
+    elif (first_person_possession == 'Gun'
+          or first_person_possession == 'Knife'):
+        print(f"{red}You've let trough a person with forbidden possessions!"
+              f"\n-$20{white}")
     else:
         print(f"{green}Good decision comrade!\n+$10{white}")
         money = money + 10
 
 
 def dontLetTrough():
+    """
+    Function which compares values between original random person and each
+    consecutive one after which it gives {money} value accordingly.
+    Called upon when replying no.
+    """
     global money
     if (random_fname_chosen == first_person_name):
-        print(f"{green}You've stopped a person with forbidden first name!\n+$30{white}")
+        print(f"{green}You've stopped a person with forbidden first name!\n"
+              f"+$30{white}")
         money = money + 30
     elif (random_lname_chosen == first_person_surname):
-        print(f"{green}You've stopped a person with forbidden last name!\n+$30{white}")
+        print(f"{green}You've stopped a person with forbidden last name!\n"
+              f"+$30{white}")
         money = money + 30
     elif (random_age == first_person_age):
-        print(f"{green}You've stopped a person with forbidden age!\n+$30{white}")
+        print(f"{green}You've stopped a person with forbidden age!\n"
+              f"+$30{white}")
         money = money + 30
     elif (random_country_chosen == first_person_country):
-        print(f"{green}You've stopped a person with forbidden origin country!\n+$30{white}")
+        print(f"{green}You've stopped a person with forbidden origin country!"
+              f"\n+$30{white}")
         money = money + 30
-    elif (first_person_possession == 'Gun' or first_person_possession == 'Knife'):
-        print(f"{green}You've stopped a person with forbidden possessions!\n+$30{white}")
+    elif (first_person_possession == 'Gun'
+          or first_person_possession == 'Knife'):
+        print(f"{green}You've stopped a person with forbidden possessions!"
+              f"\n+$30{white}")
     else:
         print(f"{red}Could've let them trough!\n-$10{white}")
         money = money - 10
 
 
 def borderCheck():
-    border_input = input(f'Do you let this person trough the border? ({green}yes{white} or {red}no{white}): \n')
+    """
+    Input function which takes values yes and no, calling previous functions
+    """
+    border_input = input(f'Do you let this person trough the border?'
+                         f'({green}yes{white} or {red}no{white}): \n')
     newLine()
     if border_input.lower() == 'yes':
         letTrough()
@@ -203,32 +268,42 @@ def borderCheck():
     else:
         print('Type yes or no')
         borderCheck()
-    
+
+
 def introduction():
     """
     Short introduction which takes the players name with a short description
     """
     newLine()
-    typingPrint(f"Zdravstvuy {red}comrade{white}, welcome to the border. What's your name? \n")
+    typingPrint(f"Zdravstvuy {red}comrade{white}, welcome to the border."
+                f"What's your name? \n")
     newLine()
     username = input("Type in your name and press return: \n")
     newLine()
-    typingPrint(f"Well officer \033[1;32m{username} \033[1;37;40m, seems like you got the short end of the stick today.\nThere are enemy spies all over the place. \n")
-    typingPrint('The boss tells us each day who we can let in the motherland based on new information. \n')
-    typingPrint('You will either earn money or lose it depending on how well you do.\n')
-    
+    typingPrint(f"Well officer \033[1;32m{username} \033[1;37;40m, seems like"
+                f" you got the short end of the stick today.\nThere are enemy"
+                f" spies all over the place. \n")
+    typingPrint('The boss tells us each day who we can let in the motherland'
+                ' based on new information. \n')
+    typingPrint('You will either earn money or lose it depending'
+                ' on how well you do.\n')
+
 
 def anotherDay():
-    play_again = input(f'Your money stays with you. \nDo you want to keep going another day? ({green}yes{white}/{red}no{white}): \n')
+    play_again = input(f'Your money stays with you. \nDo you want to keep'
+                       f' going another day? ({green}yes{white}/{red}no'
+                       f'{white}): \n')
     if play_again.lower() == 'yes':
         main()
     elif play_again.lower() == 'no':
         print('Thank you for playing!')
+        newLine()
     else:
         newLine()
         print('Type yes or no')
         newLine()
         anotherDay()
+        newLine()
 
 
 def finalScore():
@@ -237,6 +312,7 @@ def finalScore():
     else:
         print(f'{red}You have {money} dollars!{white}')
     return money
+
 
 def main():
     """
@@ -247,7 +323,7 @@ def main():
     firstPerson()
     borderCheck()
     firstPerson()
-    borderCheck() 
+    borderCheck()
     firstPerson()
     borderCheck()
     firstPerson()
@@ -257,6 +333,10 @@ def main():
     newLine()
     finalScore()
     anotherDay()
+    newLine()
+
 
 introduction()
+
+
 main()
