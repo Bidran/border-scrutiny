91be5e6758d957487263db4de4e33b34b84197c2
Bidran <>
2023-10-04 13:27:41 +0000

    Add lists containing first and last names

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time,os,sys
+
+def typingPrint(text):
+  for character in text:
+    sys.stdout.write(character)
+    sys.stdout.flush()
+    time.sleep(0.05)
+
+
+first_name_men = [
+    'Dmitri','Mikhail','Anton','Pavel','Andrei','Ivan',
+    'Sergei','Fyodor'
+    ]
+
+first_name_women = [
+    'Yelena','Natalia','Svetlana','Irina','Ekaterina',
+    'Olga','Yulia','Ana'
+    ]
+
+last_name_men = [
+    'Petrovich','Romanov','Kovalenko','Sokolov','Volkov',
+    'Nikitin','Voronin','Romanovich',
+    ]
+
+last_name_women = [
+    'Ivanova','Kuznetsova','Zhdanova','Popova','Romanovna',
+    'Ilyina','Yegorova','Titova'
+    ]
+
+
+print(first_name_men)
+print(first_name_women)
+print(last_name_men)
+print(last_name_women)
